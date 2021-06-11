7090d24321f0404d82aabaa2275a9b28bbf77e2e
rahul lour <rahullour01@gmail.com>
2021-06-11 22:19:12 +0530

    del

--- Students---LibraryManagementSystem/app/src/main/java/com/example/librarymanagementsystemstudents/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.example.librarymanagementsystemstudents;
-
-public class Utils {
-    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
-
-    public static final  String EMAIL = "msijanakpuri@gmail.com";
-    public static final String PASSWORD = "msijanakpuri@7891";
-}
