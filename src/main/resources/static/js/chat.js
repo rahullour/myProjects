ea78ce0207cfdf99601d0892b8d3d84af88a24b6
rahullour <rahullour01@gmail.com>
2025-01-14 15:57:15 +0530

    added message id, changed whole site font

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -319,7 +319,7 @@
 
     import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
     // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
-      import { getFirestore, collection, getDocs, addDoc, query, orderBy, where, limit, onSnapshot } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+      import { getFirestore, collection, getDocs, addDoc, query, orderBy, where, limit, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -361,7 +361,7 @@
                 text: messageText,
                 timestamp: new Date()
             });
-
+            await updateDoc(docRef, { messageId: docRef.id });
             console.log("Document written with ID: ", docRef.id);
 
         } catch (e) {
