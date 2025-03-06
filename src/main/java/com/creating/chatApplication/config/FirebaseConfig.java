1c8f8faf0b6a048eef2823aa5da3a55b5baca0d3
rahullour <rahullour01@gmail.com>
2025-03-06 12:01:41 +0530

    removed login with google - redirect uri requires domain

--- src/main/java/com/creating/chatApplication/config/FirebaseConfig.java
+++ src/main/java/com/creating/chatApplication/config/FirebaseConfig.java
@@ -5,9 +5,13 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,13 +19,20 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public Resource getResourceFromPath() throws IOException {
+        return resourceLoader.getResource("classpath:wechat-ec503-4b309cc2015a.json");
+    }
+
     @Bean
     public Firestore firestore() throws IOException {
         // Path to your service account key file
-        String serviceAccountPath = "/home/rahullour/Downloads/wechat-ec503-4b309cc2015a.json";
+//        String serviceAccountPath = "/src/main/resources/wechat-ec503-4b309cc2015a.json";
 
         // Use a service account
-        try (InputStream serviceAccount = new FileInputStream(serviceAccountPath)) {
+        try (InputStream serviceAccount = getResourceFromPath().getInputStream()) {
             GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(credentials)
